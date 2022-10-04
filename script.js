0f6ddc9a84a61ad61513bf3d8f74da51760a9b8e
Maksym Panasiuk <mak7devel@gmail.com>
2022-10-04 03:04:49 +0300

    added animations effects

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ buttonBlock.addEventListener('click', (e) => {
         arrItems[numberLine] = inputBlock.value;
         const newItem = document.createElement('div');
         newItem.classList.add('block__item');
+        newItem.style.height = '46px';
         listBlock.appendChild(newItem);
         const newText = document.createElement('div');
         newText.textContent = `${numberLine + 1}. ${arrItems[numberLine]}`;
@@ -27,7 +28,21 @@ buttonBlock.addEventListener('click', (e) => {
         inputBlock.value = '';
         numberLine++;
 
-        clearning.style.opacity = '1';
+        clearning.style.display = 'flex';
+        setTimeout( () => {
+            clearning.style.height = '50px';
+            clearning.style.opacity = '1';
+        }, 50);
+
+        infoBlock.textContent = 'Додано нову справу';
+        infoBlock.style.opacity = '1';
+        infoBlock.style.bottom = '-30px';
+        setTimeout( ()=>{
+            infoBlock.style.opacity = '0';
+            setTimeout( ()=> {
+                infoBlock.style.bottom = '0px';
+            }, 500);
+        }, 300);
 
     }
 });
@@ -39,6 +54,7 @@ document.addEventListener('keyup', (event) => {
             arrItems[numberLine] = inputBlock.value;
             const newItem = document.createElement('div');
             newItem.classList.add('block__item');
+            newItem.style.height = '46px';
             listBlock.appendChild(newItem);
             const newText = document.createElement('div');
             newText.textContent = `${numberLine + 1}. ${arrItems[numberLine]}`;
@@ -54,7 +70,21 @@ document.addEventListener('keyup', (event) => {
             inputBlock.value = '';
             numberLine++;
     
-            clearning.style.opacity = '1';
+            clearning.style.display = 'flex';
+            setTimeout( () => {
+                clearning.style.height = '50px';
+                clearning.style.opacity = '1';
+            }, 50);
+
+            infoBlock.textContent = 'Додано нову справу';
+            infoBlock.style.bottom = '-30px';
+            infoBlock.style.opacity = '1';
+            setTimeout( ()=>{
+                infoBlock.style.opacity = '0';
+                setTimeout( ()=> {
+                    infoBlock.style.bottom = '0px';
+                }, 500);
+            }, 300);
     
         }
     }
@@ -62,47 +92,105 @@ document.addEventListener('keyup', (event) => {
 
 document.querySelector('.block').addEventListener('click', (event) => {
     if (event.path[0].classList[0] == 'block__item' || event.path[0].classList[0] == 'block__text') {
+
         if ( event.target.style.textDecoration == 'line-through' ) {
+
             event.target.style.textDecoration = 'none'; 
+
         } else if ( event.target.style.textDecoration == '' || event.target.style.textDecoration == 'none' ) {
+
             event.target.style.textDecoration = 'line-through';
+
         }
+
     } else if ( event.path[1].classList[0] == 'block__trash' || event.path[0].classList[0] == 'block__trash' ) {
-        let textItems = event.path[2].firstChild.textContent;
-        arrItems[Number(textItems[0]) - 1] = '';
-        arrItems = arrItems.filter(Boolean);
-        numberLine--;
-        event.path[2].remove();
-        listBlock.innerHTML = '';
 
-        arrItems.forEach( ( item, i ) => {
-            const newItem = document.createElement('div');
-            newItem.classList.add('block__item');
-            listBlock.appendChild(newItem);
-            const newText = document.createElement('div');
-            newText.classList.add('block__text');
-            newText.textContent = `${i + 1}. ${arrItems[i]}`;
-            const newTrash = document.createElement('div');
-            newTrash.classList.add('block__trash');
-            const newImg = document.createElement('img');
-            newImg.src = 'trash3.svg';
-            newItem.appendChild(newText);
-            newItem.appendChild(newTrash);
-            newTrash.appendChild(newImg);
-        });
+        event.path[2].style.height = '0px';
+        event.path[2].style.marginRight = '150%';
+        event.path[2].style.marginLeft = '-150%';
 
-        if ( arrItems == '' ) {
-            clearning.style.opacity = '0';
-        }
+        setTimeout( () => {
+            let textItems = event.path[2].firstChild.textContent;
+            arrItems[Number(textItems[0]) - 1] = '';
+            arrItems = arrItems.filter(Boolean);
+            numberLine--;
+            if ( event.path[2].classList[0] == 'block__item' ) {
+                event.path[2].remove();
+            }
+            listBlock.innerHTML = '';
+
+            arrItems.forEach( ( item, i ) => {
+
+                const newItem = document.createElement('div');
+                newItem.classList.add('block__item');
+                newItem.style.height = '46px';
+                listBlock.appendChild(newItem);
+                const newText = document.createElement('div');
+                newText.classList.add('block__text');
+                newText.textContent = `${i + 1}. ${arrItems[i]}`;
+                const newTrash = document.createElement('div');
+                newTrash.classList.add('block__trash');
+                const newImg = document.createElement('img');
+                newImg.src = 'trash3.svg';
+                newItem.appendChild(newText);
+                newItem.appendChild(newTrash);
+                newTrash.appendChild(newImg);
+
+            });
+
+            if ( arrItems == '' ) {
+
+                clearning.style.height = '0px';
+                clearning.style.opacity = '0';
+                setTimeout( () => {
+                    clearning.style.display = 'none';
+                }, 600);
+
+            }
+
+            infoBlock.textContent = 'Видалено справу';
+            infoBlock.style.opacity = '1';
+            infoBlock.style.bottom = '-30px';
+
+            setTimeout( ()=>{
+                infoBlock.style.opacity = '0';
+                setTimeout( ()=> {
+                    infoBlock.style.bottom = '0px';
+                }, 500);
+            }, 300);
+        }, 600);
 
     }
 });
 
 const clearning = document.querySelector('.block__clear');
 clearning.addEventListener('click', (event) => {
+
     arrItems = [];
     numberLine = 0;
     inputBlock.value = '';
     listBlock.innerHTML = '';
+    
+    if ( clearning.style.opacity == 1 ) {
+
+        infoBlock.textContent = 'Список очищено';
+        infoBlock.style.bottom = '-30px';
+        infoBlock.style.opacity = '1';
+
+        setTimeout( ()=>{
+
+            infoBlock.style.opacity = '0';
+            setTimeout( ()=> {
+                infoBlock.style.bottom = '0px';
+            }, 500);
+
+        }, 300);
+
+    }
+
+    clearning.style.height = '0px';
     clearning.style.opacity = '0';
+    setTimeout( () => {
+        clearning.style.display = 'none';
+    }, 600);
 });
